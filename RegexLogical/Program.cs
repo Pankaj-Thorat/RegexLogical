da57815f345350a945d774042706a5409a511f8a
Pankaj-Thorat <thoratpankaj282@gmail.com>
2023-07-15 12:24:50 +0530

    [Pankaj] Added example of checking the pattern if present or not.

---
+++ RegexLogical/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexLogical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //file input
+            string path = @"C:\Users\thora\source\repos\RegexProb\RegexProb\abc.txt";
+
+            string lines;
+
+            lines = File.ReadAllText(path);
+            Console.WriteLine("Example input : " + lines);
+
+            //regex pattern
+            string pattern = @"\b\w*a(b{2,3})\w*\b";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(lines);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine("Match found : " + match.Value);
+            }
+
+        }
+    }
+}
